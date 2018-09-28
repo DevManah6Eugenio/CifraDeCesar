f29169b8ee2ca1d25bddd6455ed9fd248b2174fc
Manasses Java <manasses.java@gmail.com>
2018-09-28 13:02:15 -0300

    Create JavaIO.java
    exporta criptografia para txt

---
+++ src/cesar/JavaIO.java
@@ -0,0 +1,58 @@
+package javaio;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class JavaIO {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+//
+//        InputStream is = new FileInputStream("/home/estagiario-manha/NetBeansProjects/JavaIO/src/javaio/arquivo/arquivo.txt");
+//        InputStreamReader isr = new InputStreamReader(is);
+//        BufferedReader buffer = new BufferedReader(isr);
+//        while (buffer.ready()) {
+//            System.out.println(buffer.readLine());
+//        }
+//
+//
+//
+//        //entrada pelo teclado
+//        InputStream is = System.in;
+//        InputStreamReader isr = new InputStreamReader(is);
+//        BufferedReader br = new BufferedReader(isr);
+//        String s = null;
+//        do {
+//            s = br.readLine();
+//            System.out.println(s);
+//        } while (!s.equals(""));
+//
+//
+//
+//
+//        InputStream is = System.in;
+//        InputStreamReader isr = new InputStreamReader(is);
+//        BufferedReader br = new BufferedReader(isr);
+//
+//        OutputStream output = new FileOutputStream("/home/estagiario-manha/NetBeansProjects/JavaIO/src/javaio/arquivo/saida.txt", true);
+//        OutputStreamWriter outputwriter = new OutputStreamWriter(output);
+//        try (BufferedWriter bufferwhiter = new BufferedWriter(outputwriter)) {
+//            String text = null;
+//            do {
+//                text = br.readLine();
+//                bufferwhiter.write(text);
+//                bufferwhiter.newLine();
+//            } while (!text.equals(""));
+//        }
+//
+//        
+//
+//        Scanner s = new Scanner(System.in);
+//        PrintStream ps = new PrintStream("/home/estagiario-manha/NetBeansProjects/JavaIO/src/javaio/arquivo/saida.txt");
+//        while (s.hasNextLine()) {
+//            ps.println(s.nextLine());
+//        }
+//
+
+
+    }
+}
